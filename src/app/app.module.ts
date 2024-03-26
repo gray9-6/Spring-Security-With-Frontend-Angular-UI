b84427ae8f35be0a19ded498367a60dd689d4de8
Ajay Kumar <aky22896@gmail.com>
2024-03-26 17:49:05 +0530

    Frontend Application with Spring JWT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service/jwt.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASE_URL = "http://localhost:8080/";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JwtService {
+
+  constructor(private http: HttpClient) { }
+
+  register(signRequest: any): Observable<any> {
+    return this.http.post(BASE_URL + 'signup', signRequest);
+  }
+
+  login(loginRequest: any): Observable<any> {
+    return this.http.post(BASE_URL + 'login', loginRequest);
+  }
+
+  hello(): Observable<any> {
+    const headers = this.createAuthorizationHeader(); // Assign to a variable first
+    return this.http.get(BASE_URL + 'api/hello', { headers });
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+    const jwtToken = localStorage.getItem('jwt');
+    if (jwtToken) {
+      console.log("JWT token found in local storage", jwtToken);
+      return new HttpHeaders().set(
+        "Authorization", "Bearer " + jwtToken
+      );
+    } else {
+      console.log("JWT token not found in local storage");
+      return new HttpHeaders(); // Return empty HttpHeaders if token is not found
+    }
+  }
+}
